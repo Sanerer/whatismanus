da5d3686879bf551b78c41b7b44069d9ef69ba91
liming <feng75450@gmail.com>
2025-03-11 22:33:46 +0800

    feat: add google search code

--- src/main.js
+++ src/main.js
@@ -3,4 +3,19 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// Google Analytics
+const script1 = document.createElement('script');
+script1.async = true;
+script1.src = 'https://www.googletagmanager.com/gtag/js?id=G-1YYYDE7H1V';
+document.head.appendChild(script1);
+
+const script2 = document.createElement('script');
+script2.innerHTML = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-1YYYDE7H1V');
+`;
+document.head.appendChild(script2);
+
 createApp(App).mount('#app')
